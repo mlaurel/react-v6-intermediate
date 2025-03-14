c8bcda144e9920bcb7dd41999bc95109a715c5b9
Mike Laurel <mlaurel@hey.com>
2025-03-14 08:46:25 -0700

    Adds transitions and optimistic sample apps

--- starter/optimistic/src/App.jsx
+++ starter/optimistic/src/App.jsx
@@ -1,3 +1,74 @@
+import { useEffect, useOptimistic, useState, useTransition } from "react";
+
 export default function App() {
-  return <h1>Deep Thoughts</h1>;
+    const [isPending, startTransition] = useTransition();
+    const [thoughts, setThoughts] = useState([]);
+    const [thought, setThought] = useState("");
+    const [optimisticThoughts, addOptimisticThought] = useOptimistic(
+        thoughts,
+        (oldThoughts, newThought) => [newThought, ...oldThoughts]
+    );
+
+    async function postDeepThought() {
+        startTransition(async () => {
+            addOptimisticThought(`${thought} (Loading...)`);
+            setThought("");
+            const response = await fetch("/thoughts", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ thought }),
+            });
+            if (!response.ok) {
+                alert("This thought was not deep enough. Please try again.");
+                return;
+            }
+            const { thoughts: newThoughts } = await response.json();
+            setThoughts(newThoughts);
+        });
+    }
+
+
+    // Fetch the initial thoughts
+    // Parse the JSON response
+    // Set the thoughts state (with promises)
+    useEffect(() => {
+        fetch("/thoughts")
+            .then((res) => res.json())
+            .then((data) => {
+                setThoughts(data);
+            });
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>Deep Thoughts</h1>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    postDeepThought();
+                }}
+            >
+                <label htmlFor="thought">What's on your mind?</label>
+                <textarea
+                    id="thought"
+                    name="thought"
+                    rows="5"
+                    cols="33"
+                    value={thought}
+                    onChange={(e) => setThought(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
+            <ul>
+                {optimisticThoughts.map((thought, index) => (
+                    // {thoughts.map((thought, index) => (
+                    // Thoughts don't need to be uniqe, so this can be done with index or thought
+                    // <li key={index}>{thought}</li>
+                    <li key={index}>{thought}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
--- starter/perf/src/App.jsx
+++ starter/perf/src/App.jsx
@@ -1,3 +1,43 @@
+import { useState, useEffect, useMemo, useCallback } from "react"
+import { marked } from "marked"
+
+import MarkdownPreview from "./MarkdownPreview";
+import markdownContent from "./markdownContent"
+
 export default function App() {
-  return <h1>React Performance</h1>;
+    const [text, setText] = useState(markdownContent)
+    const [time, setTime] = useState(Date.now())
+    const [theme, setTheme] = useState("green")
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(Date.now())
+        }, 1000);
+        return () => clearInterval(interval)
+    }, [])
+
+    const options = useMemo(() => ({ text, theme }), [text, theme]);
+    // render and render2 are functionally equivalent
+    const render = useCallback((text) => marked.parse(text), []);
+    const render2 = useMemo(() => (text) => marked.parse(text), []);
+
+    return (
+        <div className="app">
+            <h1>Perf with React</h1>
+            <h2>Current Time: {time}</h2>
+            <label htmlFor="theme">
+                Choose a setTheme
+                <select onChange={e => setTheme(e.target.value)}>
+                    <option value="green">Green</option>
+                    <option value="yellow">Yellow</option>
+                    <option value="red">Red</option>
+                    <option value="violet">Violet</option>
+                </select>
+            </label>
+            <div className="markdown">
+                <textarea className="markdown-editor" value={text} onChange={(e) => setText(e.target.value)}></textarea>
+                <MarkdownPreview options={options} render={render} />
+            </div>
+        </div>
+    )
 }
